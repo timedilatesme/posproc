82b6e42ce3515b90601f9563b23c9c09a807d576
Tanmay972002 <tanmaysingh972003@yahoo.com>
2021-06-27 23:46:36 +0530

    up

--- testing/test2.py
+++ testing/test2.py
@@ -4,6 +4,10 @@ l = [1, 1, 1, 0, 1, 0, 1, 1, 0, 1]
 #random.choices([0, 1], k=10)
 print("Unshuffled: ",l)
 
-random.shuffle(l)
 
-print(l)
+mylist = random.choices([0,1],k=10)
+random.shuffle(mylist)
+
+print(mylist)
+random.shuffle(mylist)
+print(mylist)
